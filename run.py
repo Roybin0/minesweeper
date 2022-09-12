5b1f7876c09308374e3fe92f86849002ed7e259f
Robyn Smith <robyn.roybin@gmail.com>
2022-09-12 22:14:27 +0000

    Add logic to ensure that surrounding mines do not go out of range of board size

--- run.py
+++ run.py
@@ -1,16 +1,28 @@
-import random 
+import random
 
-# Create a board object to build the game 
-class Board: 
+
+# Create a board object to build the game
+class Board:
+    """
+    A board to represent the minesweeper game
+    ...
+    Attributes
+    ----------
+    difficulty: int
+        the size of the board
+    mines: int
+        the number of mines on the board
+    """
+    
     def __init__(self, difficulty, mines):
         """
         Define the board attributes
         """
         self.difficulty = difficulty
-        self.mines = mines 
+        self.mines = mines
         self.board = self.make_new_board()
         self.assign_values_to_board()
-        self.checked = set() 
+        self.checked = set()
     
     def make_new_board(self): 
         """
@@ -31,11 +43,11 @@ class Board:
         
         return board 
 
-    def assign_values_to_board(self): 
+    def assign_values_to_board(self):
         """
-        Assign a number 0-8 to any empty space to 
-        represent the number of mines in the 
-        surrounding spaces 
+        Assign a number 0-8 to any empty space to
+        represent the number of mines in the
+        surrounding spaces
         """
         for row in range(self.difficulty):
             for col in range(self.difficulty):
@@ -44,13 +56,14 @@ class Board:
     
     def get_num_surrounding_mines(self, row, col):
         """
-        Check surrounding spaces for number of mines, if any 
+        Check surrounding spaces for number of mines, if any
         """
-        surrounding_mines = 0 
-        for r in range(row-1, (row+1)+1):
-            for c in range(col-1, (col+1)+1):
-                if r == row and c == col: 
+        surrounding_mines = 0
+        for r in range(max(0, row-1), min(self.difficulty-1, row+1)+1):
+            for c in range(max(0, col-1), min(self.difficulty-1, col+1)+1):
+                if r == row and c == col:
                     continue
                 if self.board[r][c] == 'X':
                     surrounding_mines += 1
         return surrounding_mines
+
